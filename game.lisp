8df2faa7a151986cc60c8aaf4cb78b69d80b799a
Daniel Lin <danlin604@gmail.com>
2016-05-08 11:27:35 -0700

    Installed Graphviz.

--- game.lisp
+++ game.lisp
@@ -130,4 +130,17 @@
 			       (garden (living-room east door))
 			       (attic (living-room downstairs ladder))))
 
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+	(if (> (length s) *max-label-length*)
+	    (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+	    s))
+      ""))
+
 
